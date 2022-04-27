2916a4fddbcd4f441583c87561b73ceff0fdee51
unknown <=>
2022-04-27 15:42:30 +0530

    basic

---
+++ details/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, HttpResponseRedirect
+from details.forms import StudentRegistration
+from details.models import User
+# Create your views here.
+
+#this function  will add new item and show item
+def add_show(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request, 'details/addshow.html', {'form':fm, 'stu':stud})
+
+#this function will Update/Delete
+def update_data(request,id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request, 'details/updatestudent.html', {'form':fm})
+
+#this function will delete Data
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+
